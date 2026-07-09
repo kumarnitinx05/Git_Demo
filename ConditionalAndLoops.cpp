906f9e13546cfd2a1e83f1025bd86d50ecd13a19
nitinkmr-26 <nitink2465@gmail.com>
2026-07-09 21:08:10 +0530

    Initial commit

---
+++ ConditionalAndLoops.cpp
@@ -0,0 +1,64 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    //int a;
+    //cin >> a;
+
+    //cout <<"value of n is:"<< n <<endl;
+
+    //if a is positive
+    /*
+    if(a>0) {
+        cout<<" A is positive" <<endl;
+
+    }
+    else{
+        cout <<"A is negative" <<endl;
+
+    }
+   
+    int a,b;
+    cout<<"Enter the value of a "<<endl; 
+    cin>>a;
+    cout<<"Enter the value of b "<<endl;
+    cin>>b;
+
+    if(a>b){
+        cout<<" A is greater"<<endl;
+    }
+    if(a<b){
+        cout<<" B is greater"<<endl;
+
+    }
+    */
+   int a;
+   while(true){
+    cout <<"enter the value of a "<<endl;
+    cin>>a;
+
+    if(a>0){
+        cout <<" A is positive" <<endl;
+    }
+    else {
+        if(a<0){
+            cout <<"A is negative" <<endl;
+        }
+        else{
+            cout <<"A is 0" <<endl;
+        }
+    }
+}
+
+   
+
+   
+
+
+
+      
+
+     
+
+
+}
